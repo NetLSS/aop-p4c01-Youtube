f08044ce31fab9bcb5304d2f8a68be9a5712afd0
NetLSS <futureprogrammer@naver.com>
2021-07-08 19:16:07 +0900

    Feat: mainRecyclerView adapter 연결

--- app/src/main/java/com/lilcode/aop/p4c01/youtube/MainActivity.kt
+++ app/src/main/java/com/lilcode/aop/p4c01/youtube/MainActivity.kt
@@ -3,6 +3,8 @@ package com.lilcode.aop.p4c01.youtube
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.lilcode.aop.p4c01.youtube.adapter.VideoAdapter
 import com.lilcode.aop.p4c01.youtube.databinding.ActivityMainBinding
 import com.lilcode.aop.p4c01.youtube.dto.VideoDto
 import com.lilcode.aop.p4c01.youtube.service.VideoService
@@ -17,6 +19,8 @@ class MainActivity : AppCompatActivity() {
      * 먼저, 리사이클러뷰를 어뎁터와 리니어레이아웃 연결
      * 프레임 레이아웃에 프레그먼트를 어테치
      */
+    private lateinit var videoAdapter: VideoAdapter
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,6 +31,13 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
             .replace(binding.fragmentContainer.id, PlayerFragment())
             .commit()
+
+        videoAdapter = VideoAdapter()
+
+        binding.mainRecyclerView.apply {
+            adapter = videoAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
 
         getVideoList()
 
@@ -47,9 +58,10 @@ class MainActivity : AppCompatActivity() {
                             return
                         }
 
-                        response.body()?.let{
-                            Log.d("MainActiviy", it.toString())
+                        response.body()?.let{ videoDto ->
+                            videoAdapter.submitList(videoDto.videos)
                         }
+
                     }
 
                     override fun onFailure(call: Call<VideoDto>, t: Throwable) {
