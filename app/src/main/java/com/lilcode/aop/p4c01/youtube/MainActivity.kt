84193313593ea7c8e4a4ad7a1e494238f6e96530
NetLSS <futureprogrammer@naver.com>
2021-07-08 15:50:39 +0900

    Feat: 프래그 먼트 레이아웃 초기 셋팅 및 바인딩 사용 추가

--- app/src/main/java/com/lilcode/aop/p4c01/youtube/MainActivity.kt
+++ app/src/main/java/com/lilcode/aop/p4c01/youtube/MainActivity.kt
@@ -2,10 +2,22 @@ package com.lilcode.aop.p4c01.youtube
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lilcode.aop.p4c01.youtube.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    /**
+     * 먼저, 리사이클러뷰를 어뎁터와 리니어레이아웃 연결
+     * 프레임 레이아웃에 프레그먼트를 어테치
+     */
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction()
+            .replace(binding.fragmentContainer.id, PlayerFragment())
+            .commit()
     }
 }
---
+++ app/src/main/java/com/lilcode/aop/p4c01/youtube/PlayerFragment.kt
@@ -0,0 +1,7 @@
+package com.lilcode.aop.p4c01.youtube
+
+import androidx.fragment.app.Fragment
+
+class PlayerFragment : Fragment(R.layout.fragment_player){
+
+}
